982dff1f362233f009b3547429358d36eb73c13f
Lucas Bokser <lucas.bokser@le-campus-numerique.fr>
2021-06-23 14:39:02 +0200

    fin itération + memo complet

--- example-app/app/Http/Controllers/ProductController.php
+++ example-app/app/Http/Controllers/ProductController.php
@@ -8,12 +8,12 @@ class ProductController extends Controller
 {
     public function listProduct()
     {
-        return view('products.product-details');
+        return view('products.product-list');
     }
 
     public function product($id)
     {
-        return view('products.product-list',['identification' => $id]);
+        return view('products.product-details',['identification' => $id]);
     }
 }
 
